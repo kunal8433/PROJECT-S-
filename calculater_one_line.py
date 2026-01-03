733a47c4bb2e041baaf94f65654fb2d411083618
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-01-03 21:40:03 +0530

    Calculator.py

---
+++ calculater_one_line.py
@@ -0,0 +1,13 @@
+print("hii I Am kunal malik ")
+print()
+print("Welcome to Calculator One Line")
+print()
+print("1. addition")
+print("2. subtraction")
+print("3. multiplication")
+print("4. division")
+print()
+
+print(eval(input ("Enter your calculation: ")))
+#EVAL function use to calculate the expression given in string format.
+#For Example: eval("2+3") will return 5
